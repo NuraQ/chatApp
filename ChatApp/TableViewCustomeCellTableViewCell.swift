57e62cefe888615a904a87362cda6a3d9c385508
Nura <nura.qasrawi@gmail.com>
2020-10-16 14:15:40 +0300

    chat application

---
+++ ChatApp/TableViewCustomeCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCustomeCellTableViewCell.swift
+//  ChatApp
+//
+//  Created by Asal 3 on 16/10/2020.
+//  Copyright © 2020 Asal 3. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCustomeCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
